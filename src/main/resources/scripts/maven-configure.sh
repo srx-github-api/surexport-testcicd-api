1664bd595263ce7901f3337103f9a9bede051254
David Martinez <david.martinez@nubika.com>
2023-10-03 23:44:06 +0200

    maven-configure added

--- src/main/resources/scripts/maven-configure.sh
+++ src/main/resources/scripts/maven-configure.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-cp -f settings.xml $HOME/.m2/settings.xml
+MAVEN_REPOSITORY_PATH=$HOME/.m2
+if [ ! -d $MAVEN_REPOSITORY_PATH ]; then mkdir $MAVEN_REPOSITORY_PATH; fi
+cp -f settings.xml $MAVEN_REPOSITORY_PATH/settings.xml	
